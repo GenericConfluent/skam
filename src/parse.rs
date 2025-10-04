4c9685d4fdc0938ba8de6174a6ca7ff69687ba16
GenericConfluent <m13power@gmail.com>
2025-10-04 15:10:24 -0600

    feat: parser sketch

--- src/parse.rs
+++ src/parse.rs
@@ -11,6 +11,36 @@ impl<'source> ERDiagram<'source> {
     pub fn include(&mut self, source: &'source str) {
         todo!()
     }
+
+    /// document = entity_stmt | relate_stmt | isa_stmt
+    fn document() {}
+
+    /// entity_stmt = "entity" ident (attr_block | ";")
+    fn entity_stmt() {}
+
+    /// isa_stmt = "isa" ident ["(" isa_constraint {"," isa_constraint} ")"] ident {"," ident}
+    /// isa_constraint = "cover" | "overlap"
+    fn isa_stmt() {}
+
+    /// relate_stmt = "weak"? "relate" ident "(" related_entity, related_entity ("," related_entity)? ")" (attr_block | ";")
+    /// related_entity = ("require" | "single") ident
+    fn relate_stmt() {}
+
+    /// attr_block = "{" attr* "}"
+    /// attr = "key"? "set"? ident ["," ident] "\n"
+    fn attr_block() {}
+
+    /// ident = ("\"" ? any thing not " ? "\"") | (alphanumeric | "-" | "_")+
+    fn ident() {}
+}
+
+pub struct Isa<'source> {
+    superclass: &'source str,
+    subclasses: Vec<&'source str>,
+    // default: false => not every superclass is a member of a subclass
+    covering: bool,
+    // default: false => no entity in multiple subclasses
+    overlap: bool,
 }
 
 pub enum AttributeKind {
@@ -25,7 +55,12 @@ pub struct Entity<'source> {
     attribute_kind: Vec<AttributeKind>,
 }
 
+pub enum RelationConstraint {
+    Single,
+    Require,
+}
+
 pub struct Relation<'source> {
     entity: Entity<'source>,
-    relates: Vec<&'source str>,
+    relates: Vec<(&'source str, RelationConstraint)>,
 }
